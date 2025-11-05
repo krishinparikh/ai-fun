df5344f2186e5d6b6720d01040ce7d9669522e65
Krishin Parikh <krishinparikh@gmail.com>
2025-11-04 20:13:58 -0500

    added more rag functionality

--- rag_fun.py
+++ rag_fun.py
@@ -1,5 +1,6 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
 
 # Parse document
 # 
@@ -11,3 +12,24 @@ def load_documents():
     documents = loader.load()
     return documents
 
+def split_text(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=300,
+        chunk_overlap=100,
+        length_function=len,
+        add_start_index=True,
+    )
+    chunks = text_splitter.split_documents(documents)
+    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+
+    document = chunks[10]
+    print(document.page_content)
+    print(document.metadata)
+
+    return chunks
+
+def main():
+    documents = load_documents()
+    split_text(documents)
+
+main()
